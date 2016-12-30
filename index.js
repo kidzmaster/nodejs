94e263acf6039b0b7c4973b41a4ba121e93e7e74
khanhpham.ask <khanhpham.ask@gmail.com>
2016-12-30 09:07:59 +0700

    cap nhat

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app=express();
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.set("views","./views");
-app.listen(proccess.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 
 app.get("/",function(req,res){
   res.render("trangchu");
