4e02c5eccc13168a3abdc0ae0ed94550cc9eca07
khanhpham.ask <khanhpham.ask@gmail.com>
2016-12-30 09:05:55 +0700

    update2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app=express();
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.set("views","./views");
-app.listen(3000);
+app.listen(proccess.env.PORT || 3000);
 
 app.get("/",function(req,res){
   res.render("trangchu");
